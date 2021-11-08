cf7be3d9ec69ee35edd4cdf2e4e586a7bebcd609
phildavis17 <phildavis17@gmail.com>
2021-11-07 20:38:21 -0500

    work on bin search

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,6 @@
 import logging
 
-from typing import Any, Iterable
+from typing import Any, Sized
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -11,7 +11,7 @@ two_list = [1, 2]
 three_list = [1, 2, 3]
 
 
-def bin_search(l:Iterable, target: Any) -> int:
+def bin_search(l:Sized, target: Any) -> int:
     """
     If target is found, returns index of target. If not found return -1.
     """
@@ -21,7 +21,6 @@ def bin_search(l:Iterable, target: Any) -> int:
     
     while not found and high > low:
         pivot = (low + high) // 2
-        #logging.debug(f"{pivot=}")
         if l[pivot] == target:
             found = True
             break
@@ -33,6 +32,25 @@ def bin_search(l:Iterable, target: Any) -> int:
         return pivot
     else:
         return -1
+
+
+def bin_search_basic(l: Sized, target: Any) -> int:
+    """
+
+    """
+    low = 0
+    high = len(l)
+
+    while low < high:
+        mid = (high + low) // 2
+        hot = l[mid]
+        if  hot == target:
+            return mid
+        elif hot < target:
+            low = mid + 1
+        else:
+            high = mid
+        
 
 
 if __name__ == "__main__":
